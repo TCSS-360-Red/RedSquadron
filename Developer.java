349890aaed161cbe2ca91ec2ff608ddc834144a2
Brehana <ramosmiguel40@gmail.com>
2023-02-10 16:05:27 -0800

    Developer Interface created

---
+++ Developer.java
@@ -0,0 +1,14 @@
+/**
+ * Developer interface
+ * 
+ * @author Miguel Ramos
+ * @version Red.1
+ * 
+ * Interface for creating developer biographies. 
+ */
+
+
+public interface Developer {
+
+    public String getDevInfo();
+}

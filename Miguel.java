989e71283bbcb98f9ad67ce7977dfde4bff624db
Brehana <ramosmiguel40@gmail.com>
2023-02-10 16:09:58 -0800

    Implemented Developer class for Miguel

---
+++ Miguel.java
@@ -0,0 +1,6 @@
+public class Miguel implements Developer {
+    
+    public String getDevInfo() {
+        return "Miguel Ramos is one of the 4 smartest people in the world.\n The other 3 are on his development team.";
+    }
+}

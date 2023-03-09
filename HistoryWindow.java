dbf8b402d53821c135c2c8fe3aa65b2e84109af1
Jacky Fong <jackyhny@yahoo.com.hk>
2023-03-09 14:26:29 -0800

    Added Commends

--- HistoryWindow.java
+++ HistoryWindow.java
@@ -1,3 +1,11 @@
+/**
+
+HistoryWindow class for displaying the history of transactions in a scrollable panel.
+The class extends JPanel and contains a JScrollPane and a JPanel as its components.
+It provides a method to update the content of the panel based on the provided history records.
+@author Jacky Fong
+@version Red.1
+*/
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -5,6 +13,11 @@ import java.util.ArrayList;
 public class HistoryWindow extends JPanel {
     private JScrollPane scrollPane;
     private JPanel panel;
+    /**
+     * Constructor for the HistoryWindow class.
+     * 
+     * Initializes the panel and scroll pane and sets their layout and dimensions.
+     */
     public HistoryWindow() {
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
@@ -13,6 +26,14 @@ public class HistoryWindow extends JPanel {
         this.add(scrollPane, BorderLayout.CENTER);
     }
 
+    /**
+     * Updates the content of the panel based on the provided history records.
+     * 
+     * Removes all the previous components from the panel and adds new JLabels for each 
+     * history record. Each JLabel contains the task, change type, and date and time of the record.
+     * 
+     * @param theHistory an array of HistoryRecord objects to be displayed in the panel.
+     */
     public void update(HistoryRecord[] theHistory) {
         panel.removeAll();
         for (int i = 0; i < theHistory.length; i++) {
@@ -30,6 +51,11 @@ public class HistoryWindow extends JPanel {
         panel.revalidate();
     }
 
+    /**
+     * Returns the JScrollPane component of the HistoryWindow.
+     * 
+     * @return the JScrollPane component of the HistoryWindow.
+     */
     public JScrollPane getScrollPane() {
         return scrollPane;
     }
--- TaskWindow.java
+++ TaskWindow.java
@@ -1,3 +1,12 @@
+/**
+
+TaskWindow class for displaying a list of tasks in a scrollable panel.
+The class extends JPanel and contains a JScrollPane and a JPanel as its components.
+It provides a method to update the content of the panel based on the provided tasks.
+It also contains a private method to create a JPanel for each task to be displayed.
+@author Jacky Fong
+@version Red.1
+*/
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -6,6 +15,11 @@ public class TaskWindow extends JPanel {
     private JScrollPane scrollPane;
     private JPanel panel;
 
+    /**
+     * Constructor for the TaskWindow class.
+     * 
+     * Initializes the panel and scroll pane and sets their layout and dimensions.
+     */
     public TaskWindow() {
         panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
@@ -14,15 +28,31 @@ public class TaskWindow extends JPanel {
         this.add(scrollPane, BorderLayout.CENTER);
     }
 
+    /**
+     * Updates the content of the panel based on the provided tasks.
+     * 
+     * Removes all the previous components from the panel and adds a new JPanel for each 
+     * task. Each JPanel contains a JLabel with the task's name and details.
+     * 
+     * @param tasks an array of Task objects to be displayed in the panel.
+     */
     public void displayTasks(Task[] tasks) {
         panel.removeAll();
-        for (Task task : tasks) {
+        for (Task task: tasks) {
             JPanel taskPanel = createTaskPanel(task);
             panel.add(taskPanel);
         }
         panel.revalidate();
     }
 
+    /**
+     * Creates a JPanel for displaying a task.
+     * 
+     * Creates a JLabel with the task's name and adds it to a new JPanel.
+     * 
+     * @param task the Task object to be displayed in the panel.
+     * @return a new JPanel containing a JLabel with the task's name.
+     */
     private JPanel createTaskPanel(Task task) {
         JLabel taskLabel = new JLabel(task.toString());
         JPanel taskPanel = new JPanel();
@@ -30,7 +60,13 @@ public class TaskWindow extends JPanel {
         return taskPanel;
     }
 
+    /**
+     * Returns the JScrollPane component of the TaskWindow.
+     * 
+     * @return the JScrollPane component of the TaskWindow.
+     */
     public JScrollPane getScrollPane() {
         return scrollPane;
     }
+
 }

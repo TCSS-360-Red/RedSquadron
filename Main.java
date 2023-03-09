21471575284f5fd91c9605f2a99ab8b46c660cb7
Jacky Fong <jackyhny@yahoo.com.hk>
2023-03-09 13:10:11 -0800

    typo

--- Main.java
+++ Main.java
@@ -10,8 +10,8 @@ public class Main {
         JButton aboutButton = new JButton("About");
         AboutWindow myAboutWindow = new AboutWindow();
         HistoryWindow myHistoryWindow = new HistoryWindow();
-        TaskWindow myTaskWindow = new TaskWindow(); // create TaskWindow instance
-        JPanel taskPanel = new JPanel(); // create JPanel for TaskWindow and label
+        TaskWindow myTaskWindow = new TaskWindow();
+        JPanel taskPanel = new JPanel();
 
         aboutButton.setBounds(10, 10, 85, 20);
         mainFrame.setSize(450, 600);
@@ -20,13 +20,13 @@ public class Main {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.add(aboutButton, BorderLayout.NORTH);
 
-        // add label and TaskWindow to taskPanel
+        //Add TaskWindow panel to mainFrame
         JLabel taskLabel = new JLabel("Task:");
         taskPanel.setLayout(new BorderLayout());
         taskPanel.add(taskLabel, BorderLayout.NORTH);
         taskPanel.add(myTaskWindow.getScrollPane(), BorderLayout.CENTER);
 
-        // add taskPanel and HistoryWindow panel to mainFrame
+        //Add HistoryWindow panel to mainFrame
         mainFrame.add(taskPanel, BorderLayout.CENTER);
         JPanel historyPanel = new JPanel();
         JLabel historyLabel = new JLabel("History:");
